bc9122208ac5480ca880893dba3f95191809387d
Himanshu <gangravi5@gmail.com>
2020-12-21 14:16:21 +0530

    Booking Without Payment Added

---
+++ BusTicketing/src/main/java/com/coforge/controller/BookingController.java
@@ -0,0 +1,68 @@
+package com.coforge.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.coforge.model.Booking;
+import com.coforge.model.BusRoute;
+import com.coforge.model.PassangerDetails;
+import com.coforge.model.Payment;
+import com.coforge.repository.BookingRepository;
+import com.coforge.repository.BusRouteRepository;
+import com.coforge.repository.PassangerDetailsRepository;
+import com.coforge.repository.PaymentRepository;
+
+@RestController
+public class BookingController {
+
+	@Autowired
+	private BookingRepository bookingRepository;
+	@Autowired
+	private BusRouteRepository busRouteRepository;
+	@Autowired
+	private PaymentRepository paymentRepository;
+	@Autowired
+	private PassangerDetailsRepository passangerDetailsRepository;
+	
+	@GetMapping("/bookings")
+    public List<Booking> getAllBooking() {
+    	System.out.println(bookingRepository.findAll());
+        return (List<Booking>) bookingRepository.findAll();
+    }
+	
+	@GetMapping("/booking/{id}")
+    public ResponseEntity<Booking> getBusById(@PathVariable(value = "id") Long id) {
+		Optional<Booking> bookingDetails = bookingRepository.findById(id);
+        return ResponseEntity.ok().body(bookingDetails.get());
+    }
+	
+	@PostMapping("/bookings/{routeId}/{passangerId}")
+    public Booking insertBus(@RequestBody Booking bookingDetails,
+    		@PathVariable(value = "routeId") Long routeId,
+    		@PathVariable(value = "passangerId") Long passangerId) {
+		Optional<BusRoute> findRouteById = busRouteRepository.findById(routeId);
+		bookingDetails.setBusId(findRouteById.get().getBusId());
+		bookingDetails.setRouteId(findRouteById.get());
+//		Optional<Payment> findPaymentById = paymentRepository.findById(transactionId);
+//		bookingDetails.setPayment(findPaymentById.get());
+		Optional<PassangerDetails> findPassangerById = passangerDetailsRepository.findById(passangerId);
+		bookingDetails.setPassangerDetails(findPassangerById.get());
+        return bookingRepository.save(bookingDetails);
+    }
+	@PostMapping("/bookings/{}")
+	public Booking completePayment() {
+		return null;
+		
+	}
+	
+}
---
+++ BusTicketing/src/main/java/com/coforge/controller/PaymentController.java
@@ -0,0 +1,49 @@
+package com.coforge.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.coforge.model.Booking;
+import com.coforge.model.Payment;
+import com.coforge.repository.BookingRepository;
+import com.coforge.repository.PaymentRepository;
+
+@RestController
+public class PaymentController {
+
+	@Autowired
+	private PaymentRepository paymentRepository;
+	@Autowired
+	private BookingRepository bookingRepository;
+	
+	@GetMapping("/payments")
+    public List<Payment> getAllPayments() {
+    	System.out.println(paymentRepository.findAll());
+        return (List<Payment>) paymentRepository.findAll();
+    }
+	
+	@GetMapping("/payment/{id}")
+    public ResponseEntity<Payment> getPaymentById(@PathVariable(value = "id") Long id) {
+		Optional<Payment> payment = paymentRepository.findById(id);
+        return ResponseEntity.ok().body(payment.get());
+    }
+	
+	@PostMapping("/payments/{bookingId}")
+    public Payment insertPayment(@RequestBody Payment payment,
+    		@PathVariable(value = "bookingId") Long bookingId) {
+        Optional<Booking> findById = bookingRepository.findById(bookingId);
+        payment.setBookingId(findById.get());
+        //findById.get().setPayment(payment);
+		return paymentRepository.save(payment);
+    }
+	
+	
+}
